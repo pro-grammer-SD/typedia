fcb937bcf912b82e55e92550e1051279d904f1e3
pro-grammer-SD <geniussantu1983@gmail.com>
2025-04-09 19:35:35 +0530

    update even more

--- config/settings.py
+++ config/settings.py
@@ -16,6 +16,10 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Static settings
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
